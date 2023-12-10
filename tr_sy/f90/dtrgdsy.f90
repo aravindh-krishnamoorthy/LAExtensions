cd0170b3daea7b1e7222d1bb5bc1bc683dea9304
Aravindh Krishnamoorthy <aravindh.krishnamoorthy@fau.de>
2023-12-10 23:49:39 +0100

    Correct f90 code.

--- tr_sy/f90/dtrgdsy.f90
+++ tr_sy/f90/dtrgdsy.f90
@@ -6,45 +6,45 @@ double precision RL(n,n),d(n)
 !========================================
 integer i,j,k
 double precision tmp
-do concurrent (i=1:n)
+do i = 1,n
     tmp = d(i)
     d(i) = RL(i,i)
     RL(i,i) = tmp
 end do
 if (uplo.EQ.'U') then
-    do concurrent (j=1:n)
-        do concurrent (i=j+1:n)
+    do j = 1,n
+        do i = j+1,n
             RL(i,j) = 0
         end do
     end do
-    do concurrent (j = n:1:-1)
-        do concurrent (k = n:j+1:-1)
-            do concurrent (i = 1:j)
+    do j = n,1,-1
+        do k = n,j+1,-1
+            do i = 1,j
                 RL(j,i) = RL(j,i) - RL(i,k)*RL(k,j)
             end do
         end do
-        do concurrent (k = j:1:-1)
+        do k = j,1,-1
             RL(j,k) = RL(j,k)/d(k)
-            do concurrent (i = 1:k-1)
+            do i = 1,k-1
                 RL(j,i) = RL(j,i) - RL(i,k)*RL(j,k)
             end do
         end do
     end do
 else
-    do concurrent (j=1:n)
-        do concurrent (i=1:j-1)
+    do j = 1,n
+        do i= 1,j-1
             RL(i,j) = 0
         end do
     end do
-    do concurrent (j = 1:n)
-        do concurrent (k = 1:j-1)
-            do concurrent (i = j:n)
+    do j = 1,n
+        do k = 1,j-1
+            do i = j,n
                 RL(j,i) = RL(j,i) - RL(i,k)*RL(k,j)
             end do
         end do
-        do concurrent (k = j:n)
+        do k = j,n
             RL(j,k) = RL(j,k)/d(k)
-            do concurrent (i = k+1:n)
+            do i = k+1,n
                 RL(j,i) = RL(j,i) - RL(i,k)*RL(j,k)
             end do
         end do
--- tr_sy/f90/dtrtrsy.f90
+++ tr_sy/f90/dtrtrsy.f90
@@ -6,29 +6,29 @@ double precision RL(n,n),S(n,n)
 !========================================
 integer i,j,k
 if (uplo.EQ.'U') then
-    do concurrent (j = n:1:-1)
-        do concurrent (k = n:j+1:-1)
-            do concurrent (i = 1:j)
+    do j = n,1,-1
+        do k = n,j+1,-1
+            do i = 1,j
                 S(i,j) = S(i,j) - RL(i,k)*S(j,k)
             end do
         end do
-        do concurrent (k = j:1:-1)
+        do k = j,1,-1
             S(k,j) = S(k,j)/RL(k,k)
-            do concurrent (i = 1:k-1)
+            do i = 1,k-1
                 S(i,j) = S(i,j) - RL(i,k)*S(k,j)
             end do
         end do
     end do
 else
     do j = 1,n
-        do concurrent (k = 1:j-1)
-            do concurrent (i = j:n)
+        do k = 1,j-1
+            do i = j,n
                 S(i,j) = S(i,j) - RL(i,k)*S(j,k)
             end do
         end do
-        do concurrent (k = j:n)
+        do k = j,n
             S(k,j) = S(k,j)/RL(k,k)
-            do concurrent (i = k+1:n)
+            do i = k+1,n
                 S(i,j) = S(i,j) - RL(i,k)*S(k,j)
             end do
         end do

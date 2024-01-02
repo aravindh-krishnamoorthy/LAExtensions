bb286b06973499e45b7d96453316e8460e44a41d
Aravindh Krishnamoorthy <aravindh.krishnamoorthy@fau.de>
2024-01-03 00:34:34 +0100

    Updates to posi!

--- posi/posi.jl
+++ posi/posi.jl
@@ -6,7 +6,7 @@ using LinearAlgebra
 #   and Deepak Menon, arXiv:1111.4144.
 ################################################################################
 
-function posi!(X::AbstractMatrix{T}, ps::PS = NoPivot()) where {T,PS}
+function posi!(X::AbstractMatrix{T}) where {T}
     n = size(X,1)
     v = zeros(T,n,1)
     
@@ -16,29 +16,25 @@ function posi!(X::AbstractMatrix{T}, ps::PS = NoPivot()) where {T,PS}
     LAPACK.potrf!('U', X)
 
     ########################################
-    # ALGORITHM
+    # MATLAB ALGORITHM
     ########################################
-    # R(n,1:n) = R(1:n,1:n)\D(1:n,n)
+    # D = diag(1./diag(R)) ;
+    # R(n,1:n) = conj(R(1:n,1:n)\D(1:n,n)) ;
     # for i=n-1:-1:1
-    #     R(i,1:i) = R(1:i,1:i)\(D(1:i,i) - R(1:i,i+1:N)*R(i+1:N,i))
+    #     R(i,1:i) = conj(R(1:i,1:i)\(D(1:i,i) - R(1:i,i+1:N)*R(i+1:N,i))) ;
     # end
-    # R = tril(R)
+    # R = tril(R) ;
     ########################################
-
     v[n] = 1/X[n,n]
     @views LAPACK.trtrs!('U', 'N', 'N', X[1:n,1:n], v)
-    X[n,1:n] = v
+    X[n,1:n] = conj(v)
     for i=n-1:-1:1
         fill!(v, 0)
         v[i] = 1/X[i,i]
-        @views BLAS.gemm!('N', 'N', -1., X[1:i,i+1:n], X[i+1:n,i], +1., v[1:i])
+        @views BLAS.gemm!('N', 'N', T(-1), X[1:i,i+1:n], X[i+1:n,i], T(+1), v[1:i])
         @views LAPACK.trtrs!('U', 'N', 'N', X[1:i,1:i], v[1:i])
-        X[i,1:i] = v[1:i]
+        X[i,1:i] = conj(v[1:i])
     end
-    X = Hermitian(X, :L)
-    if ~isa(ps, NoPivot)
-        p = invperm(C.p)
-        X = X[p,p]
-    end
+    for i=1:n for j=i+1:n X[i,j] = X[j,i]' end end
     return X
 end

a10fa8789e8f8a5fd0c0a72852c871070d4dc379
Aravindh Krishnamoorthy <aravindh.krishnamoorthy@fau.de>
2024-01-09 20:52:43 +0100

    potri2: Test update

--- potri2/test_potri2.jl
+++ potri2/test_potri2.jl
@@ -7,29 +7,32 @@
 using LinearAlgebra
 using BenchmarkTools
 
-N = 1000
-# Real
-X = rand(N, N)
-X = X*X'
-U = Matrix(cholesky(X).U)
-X0 = LAPACK.potri!('U', copy(U))
-X1 = MatrixAlgorithms.potri2!('U', copy(U))
-display(norm(triu(X1) - X0))
-X1 = MatrixAlgorithms.dpotri2!('U', copy(U))
-display(norm(triu(X1) - X0))
-X1 = MatrixAlgorithms.dpotri2!('L', copy(U'))
-display(norm(triu(X1) - X0))
-# Timing
-@btime LAPACK.potri!('U', copy(U)) ;
-@btime MatrixAlgorithms.potri2!('U', copy(U)) ;
-@btime MatrixAlgorithms.dpotri2!('U', copy(U)) ;
-@btime MatrixAlgorithms.dpotri2!('L', copy(U')) ;
+for N in [8, 32, 64, 1000]
+    println("N=$N...")
 
-# # Complex
-# X = complex.(rand(N, N), randn(N, N))
-# X = X*X'
-# U = Matrix(cholesky(X).U)
-# X0 = LAPACK.potri!('U', copy(U))
-# X1 = MatrixAlgorithms.potri2!(copy(U))
-# display(norm(triu(X1) - X0))
+    # Real
+    X = rand(N, N)
+    X = X*X'
+    U = Matrix(cholesky(X).U)
+    L = Matrix(cholesky(X).L)
+    X0 = LAPACK.potri!('U', copy(U))
+    X1 = MatrixAlgorithms.potri2!('U', copy(U))
+    display(norm(triu(X1) - X0))
+    X1 = MatrixAlgorithms.dpotri2!('U', copy(U))
+    display(norm(triu(X1) - X0))
+    X1 = MatrixAlgorithms.dpotri2!('L', copy(L))
+    display(norm(triu(X1) - X0))
+    # Timing
+    @btime LAPACK.potri!('U', copy($U)) ;
+    @btime MatrixAlgorithms.potri2!('U', copy($U)) ;
+    @btime MatrixAlgorithms.dpotri2!('U', copy($U)) ;
+    @btime MatrixAlgorithms.dpotri2!('L', copy($L)) ;
 
+    # # Complex
+    # X = complex.(rand(N, N), randn(N, N))
+    # X = X*X'
+    # U = Matrix(cholesky(X).U)
+    # X0 = LAPACK.potri!('U', copy(U))
+    # X1 = MatrixAlgorithms.potri2!(copy(U))
+    # display(norm(triu(X1) - X0))
+end

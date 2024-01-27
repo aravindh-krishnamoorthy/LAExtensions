60be51c523dfbd56552c1359cfa9614a1bc4d672
Aravindh Krishnamoorthy <aravindh.krishnamoorthy@fau.de>
2024-01-27 18:30:06 +0100

    Intermediate test file

---
+++ potri2/mt.jl
@@ -0,0 +1,14 @@
+using LinearAlgebra
+include("../src/MatrixAlgorithms.jl")
+
+A = randn(4,4)
+A = A*A'
+
+T = Matrix(cholesky(A).U) ;
+
+T1 = LAPACK.potri!('U', copy(T))
+T1 = T1+triu(T1,1)'
+display(T1)
+T2 = MatrixAlgorithms.dpotri2!('U', copy(T))
+display(T2)
+norm(T1-T2)

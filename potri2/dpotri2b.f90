75d8f6792f909e1e8631e3dd4b7704ab7309d535
Aravindh Krishnamoorthy <aravindh.krishnamoorthy@fau.de>
2024-02-05 11:27:00 +0100

    Initial block version based on scalar

--- potri2/dpotri2b.f90
+++ potri2/dpotri2b.f90
@@ -4,18 +4,72 @@
 ! Copyright (C) 2023 Aravindh Krishnamoorthy and contributors.
 !###############################################################################
 
-SUBROUTINE DPOTRI2B(UPLO, N, A, LDA, INFO)
+SUBROUTINE DPOTRI2S(UPLO, N, A, LDA, INFO)
     IMPLICIT           NONE
 
     CHARACTER          UPLO
     INTEGER            INFO, LDA, N
     DOUBLE PRECISION   A( LDA, * )
 
+    INTEGER            I, J, K
     INTEGER            NB
+    DOUBLE PRECISION   ONE, ZERO
+    PARAMETER ( ONE = 1.0, ZERO = 0.0 )
+    PARAMETER (NB = 2)
 
-    ! Configurable block size
-    PARAMETER          (NB = 32)
-
+    IF (UPLO.EQ.'U') THEN
+        DO I = 1,N
+            A(I,I) = 1/A(I,I)
+            DO CONCURRENT (J = I+1:N)
+                A(I,J) = A(I,J)*A(I,I)
+                A(J,I) = 0
+            END DO
+            A(I,I) = A(I,I)*A(I,I)
+        END DO
+        DO J = N, 1, -1
+            DO K = N, J+1, -1
+                DO CONCURRENT (I = 1:J)
+                    A(J,I) = A(J,I) - A(I,K)*A(K,J)
+                END DO
+            END DO
+            DO K = J, 1, -1
+                DO CONCURRENT (I = 1:K-1)
+                    A(J,I) = A(J,I) - A(I,K)*A(J,K)
+                END DO
+            END DO
+        END DO
+        DO CONCURRENT (I = 1:N)
+            DO CONCURRENT (J = I+1:N)
+                A(I,J) = A(J,I)
+            END DO
+        END DO
+    ELSE ! UPLO.EQ.'L'
+        DO J = 1, N
+            A(J,J) = 1/A(J,J)
+            DO CONCURRENT (I = J+1:N)
+                A(I,J) = A(I,J)*A(J,J)
+                A(J,I) = 0
+            END DO
+            A(J,J) = A(J,J)*A(J,J)
+        END DO
+        DO J = N, 1, -1
+            DO K = N, J+1, -1
+                DO CONCURRENT (I = 1:J)
+                    A(I,J) = A(I,J) - A(K,I)*A(J,K)
+                END DO
+            END DO
+            DO K = J, 1, -1
+                DO CONCURRENT (I = 1:K-1)
+                    A(I,J) = A(I,J) - A(K,I)*A(K,J)
+                END DO
+            END DO
+        END DO
+        DO CONCURRENT (I = 1:N)
+            DO CONCURRENT (J = 1:I-1)
+                A(I,J) = A(J,I)
+            END DO
+        END DO
+    END IF
     INFO = 0
     RETURN
 END

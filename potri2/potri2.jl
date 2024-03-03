7779851275fc4ace914f8f8f485667eed06a16d7
Aravindh Krishnamoorthy <aravindh.krishnamoorthy@fau.de>
2024-03-03 17:37:08 +0100

    potri2: Minor code reorg

--- potri2/potri2.jl
+++ potri2/potri2.jl
@@ -7,6 +7,8 @@
 using LinearAlgebra
 using Libdl
 
+include("potri2_fglue.jl")
+
 ################################################################################
 # Invert a positive definite matrix using the algorithm in
 # "Matrix Inversion Using Cholesky Decomposition", by Aravindh Krishnamoorthy
@@ -67,28 +69,3 @@ function potri2!(uplo::Char, X::AbstractMatrix{T}) where {T}
     end
 end
 
-################################################################################
-# Invert a positive definite matrix using the algorithm in
-# "Matrix Inversion Using Cholesky Decomposition", by Aravindh Krishnamoorthy
-#   and Deepak Menon, arXiv:1111.4144.
-################################################################################
-# Fortran version
-################################################################################
-function dpotri2!(uplo::Char, X::AbstractMatrix{T}; rl::Bool=true) where {T}
-    # DPOTRI2(UPLO, N, A, LDA, INFO)
-    N = size(X,1)
-    INFO = Ref{Int64}()
-    if rl == true
-        lib = Libdl.dlopen("./potri2.so")
-        dpotri2 = Libdl.dlsym(lib, :dpotri2_)
-        ccall(dpotri2, Cvoid,
-            (Ref{UInt8}, Ref{Int64}, Ptr{Float64}, Ref{Int64}, Ptr{Int64}, Clong),
-            uplo, N, X, N, INFO, 1)
-        Libdl.dlclose(lib)
-    else
-        ccall((:dpotri2_, "./potri2.so"), Cvoid,
-            (Ref{UInt8}, Ref{Int64}, Ptr{Float64}, Ref{Int64}, Ptr{Int64}, Clong),
-            uplo, N, X, N, INFO, 1)
-    end    
-    return X
-end
